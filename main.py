118a1987587e2919cd5864e974270f1d1e512abd
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-12 14:28:12 +0800

    Basic pygments

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import air
 import air_convert
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
 app = air.Air()
+
+lexer = PythonLexer()
+formatter = HtmlFormatter()
+
+
+@app.post("/submit")
+async def submit(request: air.Request):
+    form = await request.form()
+    if code := form.get("code"):
+        code = air_convert.html_to_airtags(code)
+        html_code = highlight(code, lexer, formatter)
+        return air.Article(air.tags.Raw(html_code), id="result", hx_swap_oob='true')
+    return air.Children(air.Article("Nothing", id="result", hx_swap_oob=''))
 
 
 @app.page
@@ -10,12 +26,13 @@ async def index():
     return air.layouts.mvpcss(
         air.Title(title),
         air.H1(title),
+        air.Style(formatter.get_style_defs('.highlight')),
         air.Form(
             air.Textarea(
                 id="code",
                 name="code",
-                rows="6",
-                cols="60",
+                rows="8",
+                cols="80",
                 placeholder="Paste HTML here...",
                 autofocus=True,
             ),
@@ -23,14 +40,6 @@ async def index():
             hx_trigger="keyup",
             hx_swap="none",
         ),
-        air.Article("Code will go here", id="result"),
+        air.Article(air.Pre(air.Code('Code will go here')), id="result"),
     )
 
-
-@app.post("/submit")
-async def submit(request: air.Request):
-    form = await request.form()
-    if code := form.get("code"):
-        code = air_convert.html_to_airtags(code)
-        return air.Article(air.Pre(air.Code(code)), id="result", hx_swap_oob='true')
-    return air.Children(air.Article("Nothing", id="result", hx_swap_oob=''))
